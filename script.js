1636d07f7b6d51fdecdea7e64f9ea7760b24f132
Bruno Sena <103772922+brunoSena13@users.noreply.github.com>
2022-04-15 16:49:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-let btn = document.querySelector('button');
+const btn = document.querySelector('button');
 let escrita = document.querySelector('.escrita');
 btn.addEventListener('click', () => {
     escrita.innerHTML = "";
     let input = Number(document.querySelector('#numbers').value);
     if(isNaN(input)){
         alert("Digite algum número");
+    }else if(input == ""){
+        escrita.innerHTML = "";
+        alert("Qual é? Do zero é facil, digita outra coisa");
     }else{
         for(let ini = 0; ini <= 10; ini++){
             let calc = `${input} X ${ini} = ${ini*input}`;
